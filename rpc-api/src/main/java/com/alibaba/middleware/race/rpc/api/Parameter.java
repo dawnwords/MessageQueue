899632f14128bde8ad49f923dbdc15cc39c123cb
dawnwords <dawnwords@69801b0b-0531-4f3b-b608-bc6a66eaf5c0>
2015-07-27 13:04:44 +0000

    + dependency copy to target/lib

--- rpc-api/src/main/java/com/alibaba/middleware/race/rpc/api/Parameter.java
+++ rpc-api/src/main/java/com/alibaba/middleware/race/rpc/api/Parameter.java
@@ -7,8 +7,8 @@ public class Parameter {
     public static final String SERVER_IP = "127.0.0.1";
     public static final int BACKLOG_SIZE = 1024;
     public static final int SERVER_PORT = 8888;
-    public static final int SERVER_SELECTOR_THREADS = 30;
-    public static final int SERVER_WORKER_THREADS = 80;
+    public static final int SERVER_SELECTOR_THREADS = 3;
+    public static final int SERVER_WORKER_THREADS = 8;
 
     public static final int SND_BUF_SIZE = 65535;
     public static final int RCV_BUF_SIZE = 65535;
