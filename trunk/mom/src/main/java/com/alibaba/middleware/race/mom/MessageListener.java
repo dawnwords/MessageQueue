917b4e9657b2ab093837e68cd172fef286043b33
dawnwords <dawnwords@69801b0b-0531-4f3b-b608-bc6a66eaf5c0>
2015-08-05 14:18:49 +0000

    initialize mom

---
+++ trunk/mom/src/main/java/com/alibaba/middleware/race/mom/MessageListener.java
@@ -0,0 +1,9 @@
+package com.alibaba.middleware.race.mom;
+
+public interface MessageListener {
+    /**
+     * @param message
+     * @return
+     */
+    ConsumeResult onMessage(Message message);
+}
